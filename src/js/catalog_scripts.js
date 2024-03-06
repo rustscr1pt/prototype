96cae6fb19894b1b92cdd1ca6ef39ee909ec511f
Egor Ivanov <rust_lang_lover@proton.me>
2024-03-06 22:32:48 +0300

    Added green animation to buttons

--- src/js/catalog_scripts.js
+++ src/js/catalog_scripts.js
@@ -126,4 +126,17 @@ $(function() {
             });
         }
     });
+
+    $('.grid-holder-container').on('mouseenter', '.single-product-collected-container-max-width', function() {
+        const buy_button = `<div class="green-buy-button-container wow animate__animated animate__fadeInUp"><button class="buy-button-item">VIEW MORE</button></div>`
+        $(this).append(buy_button);
+    });
+
+    $('.grid-holder-container').on('mouseleave', '.single-product-collected-container-max-width', function() {
+        $(this).find(".green-buy-button-container").fadeTo(200, 0.01, function() {
+            $(this).slideUp(200, function() {
+                $(this).remove();
+            })
+        })
+    });
 })
