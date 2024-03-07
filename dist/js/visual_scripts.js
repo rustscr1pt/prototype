9c44cb945efcaa3424ab1805bb89637afe8c703f
Egor Ivanov <rust_lang_lover@proton.me>
2024-03-07 13:23:20 +0300

    //

--- dist/js/visual_scripts.js
+++ dist/js/visual_scripts.js
@@ -9,28 +9,42 @@ $(function() {
         success: function(response) {
             const doubled = response.random_positions.concat(response.random_positions);
             console.log(doubled);
-            $('.text-name-holder-container__span').each(function(index) {
-                console.log(doubled[index].name);
-                $(this).text(doubled[index].name);
-                $(this).parent().parent().find('.text-price-holder-container__span').text(doubled[index].price);
-                $(this).parent().parent().parent().find('.lot-photo-holder-container__img').attr('src', doubled[index].image_path);
-            });
-            response.available_categories.forEach(function(object, index) {
-                const to_add = `<div class="category-row-item-container wow animate__animated animate__bounceInDown">
-                         <div class="square-brackets-container">
-                             <span class="square-brackets-container__span">[0${index + 1}]</span>
-                         </div>
-                        <div class="category-name-container">
-                            <span class="category-name-container__span">${object.category.toUpperCase()}</span>
+            doubled.forEach(function(item, index) {
+                if (index < 6) {
+                    const square_element = `<div class="single-product-collected-container-max-width wow animate__animated animate__bounceInDown">
+                    <div class="grid-95-centered-holder-container__left">
+                        <span class="grid-name-title__span">${item.brand}</span>
+                    </div>
+                    <div class="lot-photo-holder-container">
+                        <img class="lot-photo-holder-container__img" src=${item.image_path} alt="">
+                    </div>
+                    <div class="double-container-holder">
+                        <div class="text-name-holder-container">
+                            <span class="text-name-holder-container__span">
+                                ${item.name}
+                            </span>
                         </div>
-                        <div class="category-items-count-container">
-                            <span class="category-items-count-container__span">${object.amount}</span>
+                        <div class="text-price-holder-container">
+                            <span class="text-price-holder-container__span">
+                                ${item.price} RUB
+                            </span>
                         </div>
-                        <div class="link-arrow-container">
-                            <span class="link-arrow-container__span">↗</span>
-                        </div>
+                    </div></div>`;
+                    $('.stock-grid-holder-container').append(square_element);
+                }
+                else if (index === 6) {
+                    const rectangle_template = `<div class="see-all-dark-rectangle-container-full wow animate__animated animate__bounceIn">
+                        <button class="see-all-dark-rectangle-container__button">
+                            <a class="see-all-dark-rectangle-container__a" href="catalog.html">
+                                SEE ALL PRODUCTS ↗
+                            </a>
+                        </button>
                     </div>`;
-                $('.categories-column-holder-container').append(to_add);
+                    $('.stock-grid-holder-container').append(rectangle_template);
+                }
+                else {
+                    return 0
+                }
             })
         },
         error: function(_, status, err) {
